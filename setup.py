e0e175ee11050ead05295749e8be7693a4ea7106
Raphaël Vinot <raphael@vinot.info>
2016-05-25 21:08:22 +0200

    Make it a package

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='pdfid',
+    version='0.2.1',
+    author='Raphaël Vinot',
+    author_email='raphael.vinot@circl.lu',
+    maintainer='Raphaël Vinot',
+    url='https://github.com/Rafiot/pdfid',
+    description='PDFiD.',
+    packages=['pdfid'],
+)
